aef17fb77796495c0ad0a4f1b630aefb05952407
Янин Ярослав <yaroslav.i.yanin@gmail.com>
2026-07-08 19:06:00 +0500

    Добавлена первоначальная структура

---
+++ app/src/main.c
@@ -0,0 +1,13 @@
+/*
+main.c - главный модуль программы. 
+
+Янин Ярослав Иванович
+*/
+
+#include "lib_main.h"
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[], char *envp[]) {
+	return 0;
+}
---
+++ lib/src/lib_main.c
@@ -0,0 +1,11 @@
+/*
+lib_main.c - главный модуль библиотеки. 
+
+Янин Ярослав Иванович
+*/
+
+#include "lib_main.h"
+
+int LibAddFunction () {
+	return 0;
+}
