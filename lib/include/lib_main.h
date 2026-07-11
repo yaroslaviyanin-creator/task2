6ecce786edd89268c2bec01f695fe5acddd5b242
Янин Ярослав <yaroslav.i.yanin@gmail.com>
2026-07-12 00:29:05 +0500

    Вынесение повторяющегося блока вывода в файл в отдельную функцию

--- lib/include/lib_main.h
+++ lib/include/lib_main.h
@@ -7,8 +7,19 @@ lib_main.h - заголовочный файл модуля lib_main.c.
 #pragma once
 #define N 4     // N - размер блока для считывания из файла
 
+#include <stdio.h>
+
 // vstrlen - длина виртуальной строки
 int vstrlen(const char* s);
+
+// Проверка символа на принадлежность к алфавиту 16 с.с.
+int ishexcode(const char c);
+
+// Превращение символа из 16 с.с. в число 10 с.с.
+int hextodec(const char c);
+
+// Функция вывода текстовых сообщений ошибок
+void print_error(const int r);
 
 // vstr_symbol - значение виртуального символа по виртуальному индексу vindex в строке s 
 char vstr_symbol(const char* s, int vindex);
@@ -27,6 +38,12 @@ size_t empty_index_mas_index(TIndex* mas_index, size_t searchlen);
 
 // Уплотняем массив (сдвигаем все непустые элементы в начало массива на место пустых)
 void compact_array(TIndex* mas_index, size_t searchlen);
+
+// Функция для отладки. Вывод массива mas_index на экран
+void print_array(TIndex* mas_index, size_t searchlen);
+
+// Посимвольный вывод строки для замены в выходной файл
+int replace_to_file(FILE* out_file, const char* replace, const size_t replacelen);
 
 // Основная логика
 int process_file(const char* in_path, const char* out_path, const char* search, const char* replace);
--- lib/src/lib_main.c
+++ lib/src/lib_main.c
@@ -6,13 +6,11 @@ lib_main.c - главный модуль библиотеки.
 
 
 #include "lib_main.h"
-#include <stdio.h>
 #include <malloc.h>
 
 // vstrlen - длина виртуальной строки
 int vstrlen(const char* s) { 
-    int i = 0;              // переменная цикла
-
+    int i = 0;              // переменная цикла (счетчик виртуальных символов)
     // Пока находятся виртуальные символы в строке, выполняем цикл
     while ((i < strlen(s)) && (vstr_symbol(s, i) != -1)) i++;
     return i;
@@ -21,8 +19,8 @@ int vstrlen(const char* s) {
 // Проверка символа на принадлежность к алфавиту 16 с.с.
 int ishexcode(const char c) {
     char ch = tolower(c);               // Преобразует заглавные латинские буквы в строчные
-    int  a = ch >= '0' && ch <= '9';
-    int  b = ch >= 'a' && ch <= 'f';
+    int  a = ch >= '0' && ch <= '9';    // a = 1, если символ ch цифра в диапазоне 0..9
+    int  b = ch >= 'a' && ch <= 'f';    // b = 1, если символ ch символ 'a', 'b', 'c', 'd', 'e' или 'f'
     if (a || b) return 1;
     else return 0;
 }
@@ -31,8 +29,8 @@ int ishexcode(const char c) {
 int hextodec(const char c) {
     char ch = tolower(c);               // Преобразует заглавные латинские буквы в строчные
     int chislo;
-    if (ch >= '0' && ch <= '9') chislo = ch - '0';
-    if (ch >= 'a' && ch <= 'f') chislo = ch - 'a' + 10;
+    if (ch >= '0' && ch <= '9') chislo = ch - '0';       // Превращение символа цифры в число
+    if (ch >= 'a' && ch <= 'f') chislo = ch - 'a' + 10;  // Превращение символа буквы в число
     return chislo;
 }
 
@@ -59,8 +57,7 @@ void print_error(const int r) {
     exit(1);  // Завершение программы
 }
 
-// vstr_symbol - значение виртуального символа по индексу vindex в строке s 
-// vindex - виртуальный индекс символа в строке s
+// vstr_symbol - значение виртуального символа по виртуальному индексу vindex в строке s 
 char vstr_symbol(const char* s, int vindex) {
     int i = 0;                      // переменная цикла
     int cur_index = 0;              // текущий индекс символа
@@ -100,32 +97,31 @@ char vstr_symbol(const char* s, int vindex) {
     return -1;
 }
 
-// Заполняем массив mas_index значением по умолчанию = -1
+// Заполняем массив структур mas_index значением по умолчанию = -1
 void init_mas_index(TIndex* mas_index, size_t searchlen) {
     for (int i = 0; i <= searchlen; i++) {
-        mas_index[i].abs_index = -1;
-        mas_index[i].search_index = -1;
+        mas_index[i].abs_index = -1;            // абсолютный индекс символа во входном файле
+        mas_index[i].search_index = -1;         // индекс в искомой строке search докуда совпала подстрока
     }
 }
 
-// Ищем в массиве mas_index первый пустой элемент (содержащий в полях значение -1)
-// Возвращаем его индекс
+// Ищем в массиве mas_index индекс первого пустого элемента (содержащего в полях значения -1)
 size_t empty_index_mas_index(TIndex* mas_index, size_t searchlen) {
     int j = 0;
     while ((mas_index[j].abs_index != -1) && (j <= searchlen)) j++;
     return j;
 }
 
-// Уплотняем массив (сдвигаем все непустые элементы вперёд на место пустых)
+// Уплотняем массив (сдвигаем все непустые элементы в начало массива на место пустых)
 void compact_array(TIndex* mas_index, size_t searchlen) {
     int cur = empty_index_mas_index(mas_index, searchlen);   // Находим первый пустой элемент
-    // И начинаем смещать следующие непустые элементы вперед массива на место пустых элементов
+    // И начинаем смещать следующие непустые элементы в начало массива на место пустых элементов
     for (int i = cur+1; i <= searchlen; i++) {
-        if (mas_index[i].abs_index != -1) {
-            // Перемещаем элемент вперед на пустую позицию
+        if (mas_index[i].abs_index != -1) {     // Если текущий элемент пустой
+            // Перемещаем элемент в начало массива на пустую позицию
             mas_index[cur].abs_index = mas_index[i].abs_index;
             mas_index[cur].search_index = mas_index[i].search_index;
-            // Очищаем элемент, из которого перемещали
+            // Очищаем содержимое элемента, из которого перемещали
             mas_index[i].abs_index = -1;
             mas_index[i].search_index = -1;
             cur++;                  // cur - позиция в массиве, куда будет записан следующий не пустой элемент
@@ -139,20 +135,30 @@ void print_array(TIndex* mas_index, size_t searchlen) {
         printf("i = %d  [ %zd ] [ %zd ]\n", y, mas_index[y].abs_index, mas_index[y].search_index);
 }
 
+// Посимвольный вывод строки для замены в выходной файл
+int replace_to_file(FILE* out_file, const char* replace, const size_t replacelen) {
+    char tmp;                               // tmp - переменная для временного символа для вывода в файл
+    for (size_t z = 0; z < replacelen; z++) {
+        tmp = vstr_symbol(replace, z);      // взятие из строки для замены вирутального символа по позиции z
+        fwrite(&tmp, 1, 1, out_file);       // запись этого символа в файл
+    }
+    return 1;   // Возращаемое значение 1 будет использоваться для установки флага подмены в 1 (flag_podmena_str = 1)
+}
+
+
 
 int process_file(const char* in_path, const char* out_path, const char* search, const char* replace) {
     
-    // Проверяем корректность введенных строк в виде аргументов, получая их виртуальные длины,
-    // с выводом соответствующих сообщений на экран.
+    // Проверяем корректность введенных строк в виде аргументов, получая их виртуальные длины
     printf("The search string");
-    int searchlen  = vstrlen(search);               // searchlen - виртуальная длина искомой строки
+    size_t searchlen  = vstrlen(search);               // searchlen - виртуальная длина искомой строки
     if (searchlen >= 0) printf(" is correct.\n");
     
     printf("The replace string");
-    int replacelen = vstrlen(replace);              // replacelen - виртуальная длина строки для замены
+    size_t replacelen = vstrlen(replace);              // replacelen - виртуальная длина строки для замены
     if (replacelen >= 0) printf(" is correct.\n");
     
-
+    
     FILE* in_file = fopen(in_path, "rb");
     if (in_file == NULL) {
         fprintf(stderr, "Error: Cannot open input file %s\n", in_path);
@@ -206,7 +212,7 @@ int process_file(const char* in_path, const char* out_path, const char* search, 
     // Работаем с блоками размера N байт из входного файла //
     //*****************************************************//
     
-    // Считываем блоки, пока не дойдем до конца
+    // Считываем блоки, пока не дойдем до конца входного файла
     while ((b_read = fread(buf, 1, N, in_file)) > 0) {
         b_count++;                             // считали очередной блок, учли его в количестве. Отсчет идёт от 0.
         for (i = 0; i < b_read; i++) {
@@ -216,68 +222,48 @@ int process_file(const char* in_path, const char* out_path, const char* search, 
 
             j = 0;                             // Переменная цикла while
 
-            // В цикле проходим по непустым элементам массива mas_index (по найденным подстрокам)
+            // В цикле проходим по непустым элементам массива mas_index (по найденным ранее подстрокам)
             while ((mas_index[j].abs_index != -1) && (j <= searchlen)) {
-                // Если символ b совпал с очередным символов строки search в уже найденной подстроке
+                // Если символ b совпал с очередным символов строки search в уже найденной подстроке,
                 if (b == vstr_symbol(search, mas_index[j].search_index + 1)) {
                     mas_index[j].search_index += 1;        // то в поле количества совпавших символов подстроки добавляем 1
-
-                    // Очередной символ строки совпал, проверяем, а не закончилась ли сверяемая строка
-                    // Если закончилась, то в выходной файл выводим подмену строки и очищаем массив индексов
-                    if (mas_index[j].search_index == searchlen-1) {
-                        // Выводим подмену строки в выходной файл
-                        for (z = 0; z < replacelen; z++) {
-                            tmp = vstr_symbol(replace, z);
-                            fwrite(&tmp, 1, 1, out_file);
-                        }
-                        init_mas_index(mas_index, searchlen);    // очищаем массив индексов
-                        flag_podmena_str = 1;                    // Устнавливаем флаг, что произошла подмена строк
+                    
+                    if (mas_index[j].search_index == searchlen-1) {  // и проверяем, а не закончилась ли сверяемая строка.
+                        // Выводим подмену строки в выходной файл и устнавливаем флаг, что произошла подмена строк
+                        flag_podmena_str = replace_to_file(out_file, replace, replacelen); 
+                        init_mas_index(mas_index, searchlen);           // очищаем массив индексов
                     }
                     j++;    // перемещаемся к следующему элементу массива mas_index 
                 }
-                else {      // Текущий символ b не совпал с ожидаемым символов из исследуемой строки
-
-                    // Записываем в выходной файл первые символы от исследуемой строки, которые вначале совпадали 
-                    if (j == 0) {   // Если это была первая подстрока, то записываем в выходной файл 
-                        // первые символы от исследуемой строки, которые вначале совпадали c исследуемой строкой
-                        // до начала второй подстроки (количество символов для записи = count_symbol) 
-                        count_symbol = mas_index[0].search_index;
-                        if (mas_index[j].abs_index != -1) count_symbol = count_symbol - mas_index[1].search_index - 1;
-                        // Запись в выходной файл
-                        for (z = 0; z <= count_symbol; z++) {
-                            tmp = vstr_symbol(search, z);
-                            fwrite(&tmp, 1, 1, out_file);
-                        }
+                else {      // Текущий символ b не совпал с ожидаемым символов из исследуемой строки.
+                    if (j == 0) {   // Если это была первая подстрока, то записываем в выходной файл первые символы от исследуемой строки,
+                        // которые в начале совпадали c исследуемой строкой, до начала второй подстроки, если она есть.
+                        count_symbol = mas_index[0].search_index;           // count_symbol - количество символов для записи из первой подстроки
+                        if (mas_index[1].abs_index != -1) count_symbol = count_symbol - mas_index[1].search_index - 1;  // есть вторая подстрока
+                        replace_to_file(out_file, search, count_symbol+1);    // Запись в выходной файл
                     }
                     // Очистка информации о данной (переставшей совпадать) подстроки в массиве mas_index.
                     mas_index[j].abs_index = -1;
                     mas_index[j].search_index = -1;
                     
-                    compact_array(mas_index, searchlen);        // Уплотнение массива (избавляемся от пустых мест сначала массива).
-                                        
-                    // Посде очистки элемента массива и его уплотнения переменная j теперь указывает на следующий по значениям элемент в этом массиве.
+                    compact_array(mas_index, searchlen);        // Уплотнение массива (избавляемся от пустых мест с начала массива).
+                   
                     // Инкрементировать значение j нет необходимости, чтобы перейти к следующему элементу массива mas_index, 
-                    // мы уже автоматически находимся на следующем элементе.
+                    // мы уже автоматически находимся на следующем элементе посде очистки элемента массива и его уплотнения.
                 }
             }
                      
             // Текущий символ совпал с началом исследуемой строки 
             if (b == vstr_symbol(search, 0)) {
                 if (searchlen == 1) {   // Если длина исследуемой строки = 1
-                    // Выводим строку для замены в выходной файл
-                    for (z = 0; z < replacelen; z++) {
-                        tmp = vstr_symbol(replace, z);
-                        fwrite(&tmp, 1, 1, out_file);
-                    }
-                    flag_podmena_str = 1;                    // Устнавливаем флаг, что произошла подмена строк
+                    // Выводим строку для замены в выходной файл и устнавливаем флаг, что произошла подмена строк
+                    flag_podmena_str = replace_to_file(out_file, replace, replacelen); 
                 }
-                else if (!flag_podmena_str) {    // Длина исследуемой строки > 1 и не было подмены строки
+                else if (!flag_podmena_str) {    // Длина исследуемой строки > 1 и не было подмены строки ранее с этим символом
                     cur_mas_index = empty_index_mas_index(mas_index, searchlen);    // Ищем первый пустой элемент в массиве mas_index
                     mas_index[cur_mas_index].abs_index = abs_cur_index;             // Записываем в него информацию о новой подстроки
                     mas_index[cur_mas_index].search_index = 0;
                 }
-          
-
             }
             
             compact_array(mas_index, searchlen);   // Уплотняем массив
@@ -289,13 +275,10 @@ int process_file(const char* in_path, const char* out_path, const char* search, 
         }
     }
 
-    // Если при завершении входного файла массив индексов mas_index не пустой, то переписываем в выходной файл начало исследуемой строки
-    // Переписываем столько символов, сколько было найденных при совпадении в первом элементе массива mas_index[0]
+    // Если при завершении входного файла массив индексов mas_index не пустой, то переписываем в выходной файл начало исследуемой строки.
+    // Переписываем столько символов, сколько было найденно при совпаденииях в первом элементе массива mas_index[0].
     if (empty_index_mas_index(mas_index, searchlen) != 0) {
-        for (z = 0; z <= mas_index[0].search_index; z++) {
-            tmp = vstr_symbol(search, z);
-            fwrite(&tmp, 1, 1, out_file);
-        }
+        replace_to_file(out_file, search, mas_index[0].search_index + 1);    // Запись в выходной файл
     }
 
     free(mas_index);        // Освобождаем память из под одномерного массива структур 
