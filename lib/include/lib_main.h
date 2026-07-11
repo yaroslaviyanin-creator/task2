74756e55e6cdbf840a85afe1cc7de813f0bb77c7
Янин Ярослав <yaroslav.i.yanin@gmail.com>
2026-07-11 20:11:11 +0500

    Полная замена подстроки для печатных символов

--- lib/include/lib_main.h
+++ lib/include/lib_main.h
@@ -5,13 +5,12 @@ lib_main.h - заголовочный файл модуля lib_main.c.
 */
 
 #pragma once
-#define N 4
+#define N 4     // N - размер блока для считывания из файла
 
 // vstrlen - длина виртуальной строки
 int vstrlen(const char* s);
 
-// vstr_symbol - значение символа по индексу vindex в строке s 
-// vindex - виртуальный индекс символа в строке s
+// vstr_symbol - значение виртуального символа по виртуальному индексу vindex в строке s 
 char vstr_symbol(const char* s, int vindex);
 
 // Структура элемента массива mas_index
@@ -20,15 +19,14 @@ typedef struct {
     size_t search_index;    // индекс в искомой строке search докуда совпала подстрока
 } TIndex;
 
-// Заполняем массив mas_index значением по умолчанию = -1
-void init_mas_index(TIndex* mas_index, size_t searchlen)
+// Заполняем массив структур mas_index значением по умолчанию = -1
+void init_mas_index(TIndex* mas_index, size_t searchlen);
 
-// Ищем в массиве mas_index первый пустой элемент (содержащий в полях значение -1)
-// Возвращаем его индекс
-size_t empty_index_mas_index(TIndex* mas_index, size_t searchlen)
+// Ищем в массиве mas_index индекс первого пустого элемента (содержащего в полях значения -1)
+size_t empty_index_mas_index(TIndex* mas_index, size_t searchlen);
 
-// Уплотняем массив (сдвигаем все непустые элементы вперёд на место пустых)
-void compact_array(TIndex* mas_index, size_t searchlen)
+// Уплотняем массив (сдвигаем все непустые элементы в начало массива на место пустых)
+void compact_array(TIndex* mas_index, size_t searchlen);
 
 // Основная логика
 int process_file(const char* in_path, const char* out_path, const char* search, const char* replace);
